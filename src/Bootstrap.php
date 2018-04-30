7bccab332ad9bd38d128b94f17b495e474c21c82
Massimo Biagioli <biagiolimassimo@gmail.com>
2018-04-30 07:25:04 +0200

    first commit

---
+++ src/Bootstrap.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+define('ROOT_DIR', dirname(__DIR__));
+
+require ROOT_DIR . '/vendor/autoload.php';
+
+\Tracy\Debugger::enable(\Tracy\Debugger::DEVELOPMENT);
+
+$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+
+$content = 'Hello ' . $request->get('name', 'visitor');
+
+$response = new \Symfony\Component\HttpFoundation\Response($content);
+$response->prepare($request);
+$response->send();
